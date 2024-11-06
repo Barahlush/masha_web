e2d1afc402d01c1ca32340e60b2e2afbdcf03648
Barahlush <baraltiva@gmail.com>
2024-11-06 15:27:14 +0500

    Initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
+import image from '@astrojs/image';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mariafedotova.com',
+  integrations: [tailwind(), icon(), sitemap(), image()]
+});
